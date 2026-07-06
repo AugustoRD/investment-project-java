a45b69cf37bf2d209dcc7191fabd7f83a5758ecf
AugustoRD <augusto06r@gmail.com>
2026-07-06 12:50:09 -0300

    feat: enhance UserServiceTest with user creation, retrieval, and listing tests

--- agregadorinvestimentos/src/test/java/com/augustord/agregadorinvestimentos/service/UserServiceTest.java
+++ agregadorinvestimentos/src/test/java/com/augustord/agregadorinvestimentos/service/UserServiceTest.java
@@ -1,12 +1,20 @@
 package com.augustord.agregadorinvestimentos.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
 
 import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -39,6 +47,7 @@ public class UserServiceTest {
     class createUser {
         
         @Test
+        @DisplayName("Should create user successfully")
         void shouldCreateUserSuccessfully() {
 
             //Arrange
@@ -63,6 +72,76 @@ public class UserServiceTest {
            
         }
 
+        @Test
+        @DisplayName("Should throw exception when error occurs")
+        void shouldThrowExceptionWhenErrorOccurs(){
+            //arrange 
+            doThrow(new RuntimeException()).when(userRepository).save(any());
+
+            var inputUser = new CreateUserDto("username", "email@example.com", "123");
+
+            //act and assert
+            assertThrows(RuntimeException.class, () -> userService.createUser(inputUser));
+            
+        }
     }
-    
+
+    @Nested
+    class getUserById{
+
+        @Test
+        @DisplayName("Should get user by id successfully when user exists")
+        void shouldGetUserByIdSuccessfully(){
+            //arrange
+            var user = new User(UUID.randomUUID(), "username", "email@example.com", "password", Instant.now(), null);
+            doReturn(Optional.of(user)).when(userRepository).findById(uuidArgumentCaptor.capture());
+
+            //act
+            var outputUser = userService.getUserById(user.getId().toString());
+
+            //assert
+            assertTrue(outputUser.isPresent());
+            assertEquals(user.getId(), uuidArgumentCaptor.getValue());
+        } 
+
+        @Test
+        @DisplayName("Should return empty when user does not exist")
+        void shouldReturnEmptyWhenUserDoesNotExist(){
+            var userid = UUID.randomUUID();
+            doReturn(Optional.empty()).when(userRepository).findById(uuidArgumentCaptor.capture());
+
+            //act
+            var outputUser = userService.getUserById(userid.toString());
+
+            //assert
+            assertFalse(outputUser.isPresent());
+            assertEquals(userid, uuidArgumentCaptor.getValue());
+
+    }
+}
+
+    @Nested
+    class listUsers{
+
+        @Test
+        @DisplayName("Should list users successfully")
+        void shouldListUsersSuccessfully(){
+            //arrange
+            var user = new User(UUID.randomUUID(), "username", "email@example.com", "password", Instant.now(), null);
+
+            var userList = List.of(user);
+            doReturn(userList)
+                    .when(userRepository)
+                    .findAll();
+
+            //act
+            var outputUsers = userService.listUsers();
+
+            //assert
+            assertNotNull(outputUsers);
+            assertEquals(userList.size(), outputUsers.size());
+            assertEquals(user.getId(), outputUsers.get(0).getId());
+
+        }
+    }
 }
